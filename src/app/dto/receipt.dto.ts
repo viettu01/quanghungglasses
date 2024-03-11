40554152c1384765f2d27e6d92e6c8161fa2c3f6
viettu01 <tupham1120@gmail.com>
2024-03-11 10:44:46 +0700

    update display list receipt

--- src/app/dto/receipt.dto.ts
+++ src/app/dto/receipt.dto.ts
@@ -3,14 +3,16 @@ import {ReceiptDetailsDto} from "./receipt-details.dto";
 
 export class ReceiptDto extends BaseDto {
   id: number;
+  status: boolean;
   staffFullname: string;
   supplierName: string;
   totalMoney: number;
   receiptDetails: ReceiptDetailsDto[];
 
-  constructor(createdDate: Date, updatedDate: Date, id: number, staffFullname: string, supplierName: string, totalMoney: number, receiptDetails: ReceiptDetailsDto[]) {
+  constructor(createdDate: Date, updatedDate: Date, id: number, status: boolean, staffFullname: string, supplierName: string, totalMoney: number, receiptDetails: ReceiptDetailsDto[]) {
     super(createdDate, updatedDate);
     this.id = id;
+    this.status = status;
     this.staffFullname = staffFullname;
     this.supplierName = supplierName;
     this.totalMoney = totalMoney;
